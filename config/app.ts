67424043a7de454e840c7a3123aec82b5c969f36
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-29 17:23:58 -0300

    fix: clear comments and switch argon2 for bcrypt

--- config/app.ts
+++ config/app.ts
@@ -1,11 +1,11 @@
-import proxyAddr from 'proxy-addr'
-import Env from '@ioc:Adonis/Core/Env'
-import { ServerConfig } from '@ioc:Adonis/Core/Server'
-import { LoggerConfig } from '@ioc:Adonis/Core/Logger'
-import { ProfilerConfig } from '@ioc:Adonis/Core/Profiler'
-import { ValidatorConfig } from '@ioc:Adonis/Core/Validator'
+import proxyAddr from "proxy-addr";
+import Env from "@ioc:Adonis/Core/Env";
+import { ServerConfig } from "@ioc:Adonis/Core/Server";
+import { LoggerConfig } from "@ioc:Adonis/Core/Logger";
+import { ProfilerConfig } from "@ioc:Adonis/Core/Profiler";
+import { ValidatorConfig } from "@ioc:Adonis/Core/Validator";
 
-export const appKey: string = Env.get('APP_KEY')
+export const appKey: string = Env.get("APP_KEY");
 
 export const http: ServerConfig = {
   allowMethodSpoofing: false,
@@ -13,43 +13,40 @@ export const http: ServerConfig = {
   subdomainOffset: 2,
 
   generateRequestId: false,
-  trustProxy: proxyAddr.compile('loopback'),
+  trustProxy: proxyAddr.compile("loopback"),
 
   etag: false,
 
-  jsonpCallbackName: 'callback',
+  jsonpCallbackName: "callback",
 
   cookie: {
-    domain: '',
-    path: '/',
-    maxAge: '2h',
+    domain: "",
+    path: "/",
+    maxAge: "2h",
     httpOnly: true,
     secure: false,
     sameSite: false,
   },
 
-  forceContentNegotiationTo: 'application/json',
-}
+  forceContentNegotiationTo: "application/json",
+};
 
 export const logger: LoggerConfig = {
-  name: Env.get('APP_NAME'),
+  name: Env.get("APP_NAME"),
 
   enabled: true,
 
+  level: Env.get("LOG_LEVEL", "info"),
 
-  level: Env.get('LOG_LEVEL', 'info'),
-
-  prettyPrint: Env.get('NODE_ENV') === 'development',
-}
+  prettyPrint: Env.get("NODE_ENV") === "development",
+};
 
 export const profiler: ProfilerConfig = {
-
   enabled: true,
 
   blacklist: [],
 
   whitelist: [],
-}
+};
 
-export const validator: ValidatorConfig = {
-}
+export const validator: ValidatorConfig = {};
